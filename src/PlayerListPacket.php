20011b8714af575a34a3cfc63de9aa3637f61896
Dylan K. Taylor <dktapps@pmmp.io>
2022-08-15 21:15:29 +0100

    Revamp PlayerListPacket

--- src/PlayerListPacket.php
+++ src/PlayerListPacket.php
@@ -15,78 +15,68 @@ declare(strict_types=1);
 namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\protocol\serializer\PacketSerializer;
-use pocketmine\network\mcpe\protocol\types\PlayerListEntry;
+use pocketmine\network\mcpe\protocol\types\PlayerListAdditionEntries;
+use pocketmine\network\mcpe\protocol\types\PlayerListAdditionEntry;
+use pocketmine\network\mcpe\protocol\types\PlayerListRemovalEntries;
 use function count;
 
 class PlayerListPacket extends DataPacket implements ClientboundPacket{
 	public const NETWORK_ID = ProtocolInfo::PLAYER_LIST_PACKET;
 
-	public const TYPE_ADD = 0;
-	public const TYPE_REMOVE = 1;
+	private const TYPE_ADD = 0;
+	private const TYPE_REMOVE = 1;
 
-	public int $type;
-	/** @var PlayerListEntry[] */
-	public array $entries = [];
+	public PlayerListAdditionEntries|PlayerListRemovalEntries $list;
 
 	/**
 	 * @generate-create-func
-	 * @param PlayerListEntry[] $entries
 	 */
-	private static function create(int $type, array $entries) : self{
+	public static function create(PlayerListAdditionEntries|PlayerListRemovalEntries $list) : self{
 		$result = new self;
-		$result->type = $type;
-		$result->entries = $entries;
+		$result->list = $list;
 		return $result;
 	}
 
-	/**
-	 * @param PlayerListEntry[] $entries
-	 */
-	public static function add(array $entries) : self{
-		return self::create(self::TYPE_ADD, $entries);
-	}
-
-	/**
-	 * @param PlayerListEntry[] $entries
-	 */
-	public static function remove(array $entries) : self{
-		return self::create(self::TYPE_REMOVE, $entries);
-	}
-
 	protected function decodePayload(PacketSerializer $in) : void{
-		$this->type = $in->getByte();
+		$type = $in->getByte();
 		$count = $in->getUnsignedVarInt();
-		for($i = 0; $i < $count; ++$i){
-			$entry = new PlayerListEntry();
-
-			if($this->type === self::TYPE_ADD){
-				$entry->uuid = $in->getUUID();
-				$entry->actorUniqueId = $in->getActorUniqueId();
-				$entry->username = $in->getString();
-				$entry->xboxUserId = $in->getString();
-				$entry->platformChatId = $in->getString();
-				$entry->buildPlatform = $in->getLInt();
-				$entry->skinData = $in->getSkin();
-				$entry->isTeacher = $in->getBool();
-				$entry->isHost = $in->getBool();
-			}else{
-				$entry->uuid = $in->getUUID();
-			}
-
-			$this->entries[$i] = $entry;
-		}
-		if($this->type === self::TYPE_ADD){
+		if($type === self::TYPE_ADD){
+			$entries = [];
 			for($i = 0; $i < $count; ++$i){
-				$this->entries[$i]->skinData->setVerified($in->getBool());
+				$uuid = $in->getUUID();
+				$actorUniqueId = $in->getActorUniqueId();
+				$username = $in->getString();
+				$xboxUserId = $in->getString();
+				$platformChatId = $in->getString();
+				$buildPlatform = $in->getLInt();
+				$skinData = $in->getSkin();
+				$isTeacher = $in->getBool();
+				$isHost = $in->getBool();
+				$entries[$i] = new PlayerListAdditionEntry($uuid, $actorUniqueId, $username, $skinData, $xboxUserId, $platformChatId, $buildPlatform, $isTeacher, $isHost);
 			}
+
+			for($i = 0; $i < $count; ++$i){
+				$entries[$i]->skinData->setVerified($in->getBool());
+			}
+
+			$this->list = new PlayerListAdditionEntries($entries);
+		}elseif($type === self::TYPE_REMOVE){
+			$entries = [];
+			for($i = 0; $i < $count; ++$i){
+				$entries[] = $in->getUUID();
+			}
+
+			$this->list = new PlayerListRemovalEntries($entries);
+		}else{
+			throw new PacketDecodeException("Unknown PlayerListPacket type $type");
 		}
 	}
 
 	protected function encodePayload(PacketSerializer $out) : void{
-		$out->putByte($this->type);
-		$out->putUnsignedVarInt(count($this->entries));
-		foreach($this->entries as $entry){
-			if($this->type === self::TYPE_ADD){
+		$out->putByte($this->list instanceof PlayerListAdditionEntries ? self::TYPE_ADD : self::TYPE_REMOVE);
+		$out->putUnsignedVarInt(count($this->list->entries));
+		if($this->list instanceof PlayerListAdditionEntries){
+			foreach($this->list->entries as $entry){
 				$out->putUUID($entry->uuid);
 				$out->putActorUniqueId($entry->actorUniqueId);
 				$out->putString($entry->username);
@@ -96,13 +86,14 @@ class PlayerListPacket extends DataPacket implements ClientboundPacket{
 				$out->putSkin($entry->skinData);
 				$out->putBool($entry->isTeacher);
 				$out->putBool($entry->isHost);
-			}else{
-				$out->putUUID($entry->uuid);
 			}
-		}
-		if($this->type === self::TYPE_ADD){
-			foreach($this->entries as $entry){
+
+			foreach($this->list->entries as $entry){
 				$out->putBool($entry->skinData->isVerified());
+			}
+		}else{
+			foreach($this->list->entries as $entry){
+				$out->putUUID($entry);
 			}
 		}
 	}
