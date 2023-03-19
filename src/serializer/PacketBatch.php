6ca1e90bcda09866fb9cb6e77b2eeb2bf93fd2fc
DriesC-2158300 <coenendries3@gmail.com>
2023-03-19 20:23:03 +0100

    Fix PHPStan

--- src/serializer/PacketBatch.php
+++ src/serializer/PacketBatch.php
@@ -56,13 +56,13 @@ class PacketBatch{
 	 * @phpstan-return \Generator<int, Packet, void, void>
 	 * @throws PacketDecodeException
 	 */
-	final public static function decodePackets(int $protocolId, BinaryStream $stream, PacketSerializerContext $context, PacketPool $packetPool) : \Generator{
+	final public static function decodePackets(BinaryStream $stream, PacketSerializerContext $context, PacketPool $packetPool) : \Generator{
 		$c = 0;
 		foreach(self::decodeRaw($stream) as $packetBuffer){
 			$packet = $packetPool->getPacket($packetBuffer);
 			if($packet !== null){
 				try{
-					$packet->decode(PacketSerializer::decoder($packetBuffer, 0, $context, $protocolId));
+					$packet->decode(PacketSerializer::decoder($packetBuffer, 0, $context));
 				}catch(PacketDecodeException $e){
 					throw new PacketDecodeException("Error decoding packet $c in batch: " . $e->getMessage(), 0, $e);
 				}
@@ -78,9 +78,9 @@ class PacketBatch{
 	 * @param Packet[]       $packets
 	 * @phpstan-param list<Packet> $packets
 	 */
-	final public static function encodePackets(int $protocolId, BinaryStream $stream, PacketSerializerContext $context, array $packets) : void{
+	final public static function encodePackets(BinaryStream $stream, PacketSerializerContext $context, array $packets) : void{
 		foreach($packets as $packet){
-			$serializer = PacketSerializer::encoder($context, $protocolId);
+			$serializer = PacketSerializer::encoder($context);
 			$packet->encode($serializer);
 			$stream->putUnsignedVarInt(strlen($serializer->getBuffer()));
 			$stream->put($serializer->getBuffer());
@@ -119,10 +119,10 @@ class PacketBatch{
 	 * @deprecated
 	 * Constructs a packet batch from the given list of packets.
 	 */
-	public static function fromPackets(int $protocolId, PacketSerializerContext $context, Packet ...$packets) : self{
+	public static function fromPackets(PacketSerializerContext $context, Packet ...$packets) : self{
 		$stream = new BinaryStream();
 		// @phpstan-ignore-next-line
-		self::encodePackets($protocolId, $stream, $context, $packets);
+		self::encodePackets($stream, $context, $packets);
 		return new self($stream->getBuffer());
 	}
 
--- tests/phpunit/serializer/PacketBatchTest.php
+++ tests/phpunit/serializer/PacketBatchTest.php
@@ -24,7 +24,6 @@ declare(strict_types=1);
 namespace pocketmine\mcpe\protocol\serializer;
 
 use PHPUnit\Framework\TestCase;
-use pocketmine\network\mcpe\convert\GlobalItemTypeDictionary;
 use pocketmine\network\mcpe\protocol\PacketDecodeException;
 use pocketmine\network\mcpe\protocol\PacketPool;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
@@ -40,7 +39,7 @@ class PacketBatchTest extends TestCase{
 	public function testDecodeTooBig() : void{
 		$limit = 10;
 		$decoderContext = new PacketSerializerContext(new ItemTypeDictionary([new ItemTypeEntry("minecraft:shield", 0, false)]));
-		$write = PacketBatch::fromPackets(ProtocolInfo::CURRENT_PROTOCOL, $decoderContext, ...array_fill(0, $limit + 1, new TestPacket()));
+		$write = PacketBatch::fromPackets($decoderContext, ...array_fill(0, $limit + 1, new TestPacket()));
 		$read = new PacketBatch($write->getBuffer());
 		$this->expectException(PacketDecodeException::class);
 		$readCount = 0;
@@ -52,7 +51,7 @@ class PacketBatchTest extends TestCase{
 	public function testDecodeAtLimit() : void{
 		$limit = 10;
 		$decoderContext = new PacketSerializerContext(new ItemTypeDictionary([new ItemTypeEntry("minecraft:shield", 0, false)]));
-		$write = PacketBatch::fromPackets(ProtocolInfo::CURRENT_PROTOCOL, $decoderContext, ...array_fill(0, $limit, new TestPacket()));
+		$write = PacketBatch::fromPackets($decoderContext, ...array_fill(0, $limit, new TestPacket()));
 		$read = new PacketBatch($write->getBuffer());
 		$readCount = 0;
 		foreach($read->getPackets(PacketPool::getInstance(), $decoderContext, $limit) as $packet){
